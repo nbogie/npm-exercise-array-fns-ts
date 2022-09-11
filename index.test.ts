f931b121f45c8e41628d661a94507dc2c84b0f4f
nbogie <neillbogie@googlemail.com>
2022-09-11 05:01:19 +0100

    some types for index.test.ts

--- index.test.ts
+++ index.test.ts
@@ -1,22 +1,22 @@
-const { collect, repeat, zip } = require("./index");
+import { collect, repeat, zip } from "./index";
 
 test("collect works with a function that uses the arg", () => {
-  expect(collect(3, (ix) => ix * 10)).toStrictEqual([0, 10, 20]);
+  expect(collect(3, (ix: number) => ix * 10)).toStrictEqual([0, 10, 20]);
 });
 test("collect works with 0 elems", () => {
-  expect(collect(0, (ix) => ix * 10)).toStrictEqual([]);
+  expect(collect(0, (ix: number) => ix * 10)).toStrictEqual([]);
 });
 
 test("repeat works with 0 reps", () => {
-  const arr = [];
-  const res = repeat(0, (ix) => arr.push(ix * 10));
+  const arr: any[] = [];
+  const res = repeat(0, (ix: number) => arr.push(ix * 10));
   expect(res).toBe(undefined);
   expect(arr).toStrictEqual([]);
 });
 
 test("repeat works with 3 mutation reps", () => {
-  const arr = [];
-  repeat(3, (ix) => arr.push(ix * 10));
+  const arr: number[] = [];
+  repeat(3, (ix: number) => arr.push(ix * 10));
   expect(arr).toStrictEqual([0, 10, 20]);
 });
 
